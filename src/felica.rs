e7fb6f089fd8b010a82d769e147410862b3bee0b
Misty De Meo <mistydemeo@gmail.com>
2021-05-25 23:32:12 -0700

    Add initial docs

--- src/felica.rs
+++ src/felica.rs
@@ -1,14 +1,24 @@
 use pafe_sys;
 
+/// Represents data read from a FeliCa card.
+/// While this includes a few helper methods, most consumers will
+/// want to read the data directly from the underlying [pafe_sys::tag_felica](pafe_sys::tag_felica) struct.
 pub struct FelicaTag {
+    /// The raw pafe_sys::tag_felica](pafe_sys::tag_felica) struct
+    /// returned by libpafe. Its members are exactly the same as in
+    /// the C library. For more details, please consult [libpafe](https://github.com/rfujita/libpafe).
     pub tag: pafe_sys::Felica,
 }
 
 impl FelicaTag {
+    /// Extracts just the manufacturer code from the card's IDm.
+    /// The IDm is eight bytes; the manufacturer code is the first two bytes.
     pub fn manufacturer_code(&self) -> [u8; 2] {
         [self.tag.IDm[0], self.tag.IDm[1]].clone()
     }
 
+    /// Extracts just the card ID from the card's IDm.
+    /// The IDm is eight bytes; the card ID is the last six bytes.
     pub fn card_id(&self) -> &[u8] {
         &self.tag.IDm[2..7]
     }
--- src/pasori.rs
+++ src/pasori.rs
@@ -1,6 +1,9 @@
 use crate::felica::FelicaTag;
 use pafe_sys;
 
+/// Specifies the timeslot to use when polling.
+/// This affects the number of cards which can be read at once.
+/// For more information, see [Sony's documentation](https://www.sony.net/Products/felica/business/tech-support/data/card_usersmanual_2.11e.pdf).
 pub enum Timeslot {
     N0,
     N1,
@@ -12,6 +15,8 @@ pub enum Timeslot {
 // Taken from this document; these are the only supported values.
 // https://www.sony.net/Products/felica/business/tech-support/data/card_usersmanual_2.11e.pdf
 impl Timeslot {
+    /// Converts a Timeslot variant into a u8 suitable for use with
+    /// the `pafe_sys::felica_polling` function.
     pub fn as_u8(&self) -> u8 {
         match self {
             Self::N0 => 0,
@@ -23,13 +28,20 @@ impl Timeslot {
     }
 }
 
+/// Specifies the type of card to interact with.
+/// Primarily used by functions that read or write data.
 pub enum CardType {
+    /// Any card
     Any,
+    /// Rakuten Edy
     Edy,
+    /// JR East Suica
     Suica,
 }
 
 impl CardType {
+    /// Converts a CardType variant into constants suitable for use with
+    /// `pafe_sys` functions.
     pub fn to_sys(&self) -> u16 {
         match self {
             Self::Any => pafe_sys::FELICA_POLLING_ANY,
@@ -39,11 +51,20 @@ impl CardType {
     }
 }
 
+/// Represents an individual PaSoRi reader.
+/// This can be created by calling Pasori::create().
+/// Methods on this struct will interact with the same PaSoRi reader it was created from.
 pub struct Pasori {
     pointer: *mut pafe_sys::Pasori,
 }
 
 impl Pasori {
+    /// Creates a handle representing a specific PaSoRi reader.
+    /// Due to a limitation in the underlying library, it's not possible
+    /// to choose a specific reader if more than one is attached to the
+    /// computer.
+    /// The reader won't yet be initialized after calling this; make sure
+    /// to call `.init()` before calling any methods.
     pub fn create() -> Pasori {
         let pasori;
         unsafe {
@@ -53,6 +74,9 @@ impl Pasori {
         Pasori { pointer: pasori }
     }
 
+    /// Attempts to initialize the PaSoRi represented by this struct.
+    /// Returns Err if it couldn't be initialized; otherwise it returns
+    /// self, which is suitable for chaining.
     pub fn init(&self) -> Result<&Self, String> {
         let result;
         unsafe {
@@ -66,6 +90,18 @@ impl Pasori {
         }
     }
 
+    /// Attempts to read a card within NFC distance. If successful, returns
+    /// a [FelicaTag](FelicaTag) containing the card's data.
+    /// This method attempts to read the card immediately, then returns;
+    /// it won't poll continuously. Most likely, you'll want to call this
+    /// method in a loop.
+    ///
+    /// `card_type` allows you to specify what kinds of card to read.
+    /// For example, if `CardType::Suica` is passed, an Edy will be ignored
+    /// if it's tapped.
+    /// The `timeslot` parameter affects the number of cards that can be
+    /// read at once; for more information, see [Timeslot](Timeslot) and
+    /// the FeliCa specification.
     pub fn poll(&self, card_type: CardType, timeslot: Timeslot) -> Option<FelicaTag> {
         let card_type_raw = card_type.to_sys();
         let pointer;
